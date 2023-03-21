d0f436266af8bf1f5cf7ce7d9fa91dd49443bf8f
Blackarrow299 <yassine.goumni@gmail.com>
2023-03-21 22:39:00 +0000

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,6 +4,11 @@
 int main(void)
 {
     char source[] = "_putchar";
-    int i for (i = 0; i < sizeof(source); i++){
-        _putchar(source[i])} _putchar("\n") return (0)
+    int i;
+    for (i = 0; i < sizeof(source); i++)
+    {
+        _putchar(source[i]);
+    }
+    _putchar("\n");
+    return (0)
 }
