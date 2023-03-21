c1642c677cb25beea5e6894fdd61e2fbef130978
Blackarrow299 <yassine.goumni@gmail.com>
2023-03-21 22:38:08 +0000

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+
+#include "main.h"
+
+int main(void)
+{
+    char source[] = "_putchar";
+    int i for (i = 0; i < sizeof(source); i++){
+        _putchar(source[i])} _putchar("\n") return (0)
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-low_level_programmingread me
+# alx-low_level_programming
