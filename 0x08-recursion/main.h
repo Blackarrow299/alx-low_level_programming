f050660b107462a1269fa66f50ae71a5a30dd068
Blackarrow299 <yassine.goumni@gmail.com>
2023-04-05 18:06:47 +0000

    2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,2 +1,13 @@
-void _puts_recursion(char *s)
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+/**
+ *void prototypes(void)
+ *int prototypes(int)
+ *char prototypes(char)
+ */
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif /* MAIN_H */
