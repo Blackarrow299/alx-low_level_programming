aebab1cc81c0538602a9612053837d1305582699
Blackarrow299 <yassine.goumni@gmail.com>
2023-04-11 00:05:35 +0000

    4

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
  *@argv: array of arguments
  *Return: 0
  */
-int main(int argc, char *argv[]) {
-(void) argc;
-
+int main(int argc, char *argv[]) 
+{
+(void)argc;
 printf("%s\n", argv[0]);
-
 return (0);
 }
+
 
