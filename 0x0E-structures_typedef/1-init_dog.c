ab88b3e8bc23c2515191fb6b3b374c3bd0269e5a
Blackarrow299 <yassine.goumni@gmail.com>
2023-04-18 00:41:35 +0000

    2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,19 @@
+#include "dog.h"
+
 /**
- * init_dog - init dog
- * @dog: First operand
- * @name: Second operand
- * @age: Second operand
- * @owner:
- * Description: This is a longer description.
- * Return: void
+ * init_dog - inits a variable of
+ *type dog
+ *@d: dog identification
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner's name
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner) {
-	d->name = *name;
-	d->age = age;
-	d->owner = *owner;
+if(d != NULL) {
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,9 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - Poppy
  * @name: string
@@ -12,4 +18,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+#endif
