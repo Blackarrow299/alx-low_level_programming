30671340d928045c8af7a88a878a02e3852fc269
Blackarrow299 <yassine.goumni@gmail.com>
2023-04-18 00:51:13 +0000

    9

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,4 +27,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog dog_t;
 void print_dog(struct dog *d);
 
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 #endif
