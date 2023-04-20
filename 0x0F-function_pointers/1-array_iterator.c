4097abe03c0f3c566150c73774355cc964b6aaad
Blackarrow299 <yassine.goumni@gmail.com>
2023-04-20 17:16:14 +0000

    4

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,8 +8,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+if (array == NULL || action == NULL)
+return;
 for (size_t i = 0; i < size; i++)
 {
-action(i);
+action(*(array + i));
 }
 }
