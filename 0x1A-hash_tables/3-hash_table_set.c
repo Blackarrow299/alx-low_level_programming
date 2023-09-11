9be57c63f74e32678e1f35aba0badb3ce8b03b26
Blackarrow299 <yassine.goumni@gmail.com>
2023-09-12 00:48:13 +0100

    0x1A

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,61 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add the element to.
+ * @key: The key of the element.
+ * @value: The value of the element.
+ *
+ * Return: 1 if successful, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int index;
+	hash_node_t *node;
+	char *valueCopy;
+	unsigned int i;
+
+	if (key == NULL || key[0] == '\0' || value == NULL || ht == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	valueCopy = strdup(value);
+	if (valueCopy == NULL)
+		return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(valueCopy);
+		return (0);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(valueCopy);
+		free(node);
+		return (0);
+	}
+	node->value = valueCopy;
+	if (ht->array[index] != NULL)
+	{
+		for (i = index; ht->array[i]; i++)
+		{
+			if (strcmp(ht->array[i]->key, key) == 0)
+			{
+				free(ht->array[i]->value);
+				ht->array[i]->value = valueCopy;
+				free(node->key);
+				free(node);
+				return (1);
+			}
+		}
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+	else
+	{
+		ht->array[index] = node;
+	}
+	return (1);
+}
+
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key in a hash table.
+ * @ht: The hash table to search.
+ * @key: The key to search for.
+ *
+ * Return: The value associated with the key, or NULL if the key is not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned int i;
+
+	if (!key || key[0] == '\0' || !ht || !ht->array[index])
+		return (NULL);
+
+	for (i = index; ht->array[index]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+			return (ht->array[i]->value);
+	}
+
+	return (NULL);
+}
